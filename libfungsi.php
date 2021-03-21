ae0fe8c1ed55fd884de1ee644f99bcd9da02cd10
Jokosupriyanto22 <jokobaeh99@gmail.com>
2021-03-21 19:43:51 +0700

    FIX

--- libfungsi.php
+++ libfungsi.php
@@ -3,26 +3,26 @@
 function grade($nilai_akhir)
 {
     if (($nilai_akhir >= 0) && ($nilai_akhir <= 35)) {
-        return print("E");
+        return "E";
     } else if (($nilai_akhir >= 36) && ($nilai_akhir <= 55)) {
-        return print("D");
+        return "D";
     } else if (($nilai_akhir >= 56) && ($nilai_akhir <= 69)) {
-        return print("C");
+        return "C";
     } else if (($nilai_akhir >= 70) && ($nilai_akhir <= 84)) {
-        return print("B");
+        return "B";
     } else if (($nilai_akhir >= 85) && ($nilai_akhir <= 100)) {
-        return print("A");
+        return "A";
     } else if (($nilai_akhir < 0) && ($nilai_akhir > 100)) {
-        return print("I");
+        return "I";
     }
 }
 
 function kelulusan($nilai_akhir)
 {
     if ($nilai_akhir > 55) {
-        return print("LULUS");
+        return "LULUS";
     } else {
-        return print("TIDAK LULUS");
+        return "TIDAK LULUS";
     }
 }
 
@@ -30,22 +30,22 @@ function predikat($nilai_akhir)
 {
     switch ($nilai_akhir) {
         case ($nilai_akhir >= 0) && ($nilai_akhir <= 35):
-            print("Sangat Kurang");
+            return "Sangat Kurang";
             break;
         case ($nilai_akhir >= 36) && ($nilai_akhir <= 55):
-            print("Kurang");
+            return "Kurang";
             break;
         case ($nilai_akhir >= 56) && ($nilai_akhir <= 69):
-            print("Cukup");
+            return "Cukup";
             break;
         case ($nilai_akhir >= 70) && ($nilai_akhir <= 84):
-            print("Memuaskan");
+            return "Memuaskan";
             break;
         case ($nilai_akhir >= 85) && ($nilai_akhir <= 100):
-            print("Sangat cukup");
+            return "Sangat cukup";
             break;
         case ($nilai_akhir < 0) && ($nilai_akhir > 100):
-            print("Tidak Ada");
+            return "Tidak Ada";
             break;
     }
 }
